d36d7be3c675852e78c4b740596261343ebed679
Andrei Baicus <baicusandrei@me.com>
2018-03-22 11:47:18 +0200

    Version bump to 1.0.1

--- class-elementor-extra-widgets.php
+++ class-elementor-extra-widgets.php
@@ -22,7 +22,7 @@ if ( ! class_exists( '\ThemeIsle\ElementorExtraWidgets' ) ) {
 		 * The version of this library
 		 * @var string
 		 */
-		public static $version = '1.0.0';
+		public static $version = '1.0.1';
 
 		/**
 		 * Defines the library behaviour
